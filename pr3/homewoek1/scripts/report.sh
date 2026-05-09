3a6a0bc3ca2133cf132aeea98d8e76736eb19282
fulla77 <fulla03.hamedo@gmail.com>
2026-05-10 01:53:16 +0300

    renamed

--- pr3/homewoek1/scripts/report.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FILENAME="notes/report.txt"
-
-echo "User: $(whoami)" > $FILENAME
-echo "Directory: $(pwd)" >> $FILENAME
-echo "Files: $(ls data | wc -l)" >> $FILENAME
-
-echo "Script executed."
